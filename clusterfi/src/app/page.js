cab03ab33aa217763006e4a8edc95ba10ecbd8df
FrankPak <francois.pakkanen@gmail.com>
2024-01-26 10:41:44 +0200

    made working ts particles and added couple pages

--- clusterfi/src/app/page.js
+++ clusterfi/src/app/page.js
@@ -1,25 +1,61 @@
 "use client"
 import Image from "next/image";
-import { useCallback } from "react";
 import ParticlesBackground from "./components/ParticlesBackground";
-import particlesConfig from "./config/particles-config";
 
-import { useEffect, useMemo, useState } from "react";
-import Particles, { initParticlesEngine } from "@tsparticles/react";
-// import { loadAll } from "@/tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
-// import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
-import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
-// import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
 
 
 export default function Home() {
   return(
-    <main className="flex justify-center height-lvh">
+    <main className="flex-col">
       {/*Custom Header with pages*/}
       {/*Cluster image*/}
-      <div className="height-lvh w-1/2">
-        <ParticlesBackground/>
+      <div className="h-[900px] w-full relative flex justify-center">
+        <div 
+        className="flex justify-center absolute inset-0">
+            <ParticlesBackground 
+            className=""/>
+        </div>
+        <Image src="/clusterLogo.png"
+        width={500}
+        height={500} 
+        className="absolute top-1/4">
+        </Image>
       </div>
+      <div className="flex justify-center text-wrap">
+        <div className="grid grid-rows-1 w-1/2 content-center">
+          <article className="text-wrap">
+          <p3>Hei, Maailma!</p3>
+          <p className="py-10">Cluster ry on LUT-yliopiston tietotekniikan opiskelijoiden yhdistys. Cluster ry on perustettu vuonna 1985
+           ja se on Suomen vanhin tietotekniikan kilta. Parhaiten clusterilaisen tunnistaa kirkkaanpunaisista
+           haalareista ja mieltymyksestä keskusteluun tietokoneiden saloista.</p>
+
+           <p>Cluster ry:n tarkoituksena on yhdistää LUT:in tietotekniikan opiskelijoita sekä tuottaa jäsenistölleen opintoihin ja vapaa-aikaan liittyviä palveluja.
+            Kilta järjestää mm. rekrytilaisuuksia yhteistyössä IT-alan yritysten kanssa, excursioita ja muita pienimuotoisempia tapahtumia ja palveluja.
+            Myös viihteellisempää toimintaa järjestetään silloin tällöin erilaisten bileiden muodossa.
+            Cluster tekee myös tiiviisti yhteistyötä LUT:in tietotekniikan koulutusohjelman kanssa mm. opintoihin liittyvissä asioissa.</p>
+           
+           <p className="py-10">Clusterin virallinen irc-kanava: #cluster @ IRCnet.</p>
+           
+           <p>PS: Clusterin punainen on #d41f2cff tai toisin sanottuna cmyk 0 85 79 17</p>
+           </article>
+        </div>
+      </div>
+      <div className="flex flex-col justify-center">
+        <p>Sponsors:</p>
+        <ul className="grid grid-cols-4 grid-rows-3 gap-1">
+          <li>Anything you want</li>
+          <li>Anything you want</li>
+          <li>Anything you want</li>
+          <li>Anything you want</li>
+          <li>Anything you want</li>
+          <li>Anything you want</li>
+          <li>Anything you want</li>
+          <li>Anything you want</li>
+          <li>Anything you want</li>
+        </ul>
+      </div>
+
+
 
       {/*Custom Header*/}
 
---
+++ clusterfi/src/app/pages/clusterRy/page.js
@@ -0,0 +1,302 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="p-24" >
+        <p className="text-2xl">Cluster 2024</p> {/*Koodi tyylisesti?*/}
+        <p>Hallitus</p>
+
+        <div className="grid w-full grid-cols-4 gap-x-7 gap-y-20 ">
+          <div>
+          <Image src="/nerd.jpg"
+            width={500}
+            height={500}/>
+            <p>Name</p>
+            <p>Puheenjohtaja</p>
+            <p>pj ät cluster.fi</p>
+          </div>
+
+          <div>
+          <Image src="/nerd.jpg"
+            width={500}
+            height={500}/>
+            <p>Name</p>
+            <p>Varapuheenjohtaja</p>
+            <p>pj ät cluster.fi</p>
+            <p></p>
+          </div>
+
+          <div>
+          <Image src="/nerd.jpg"
+            width={500}
+            height={500}/>
+            <p>Name</p>
+            <p>Projektivastaava</p>
+            <p>projekti ät cluster.fi</p>
+          </div>
+
+          <div>
+          <Image src="/nerd.jpg"
+            width={500}
+            height={500}/>
+            <p>Name</p>
+            <p>Rahastonhoitaja</p>
+            <p>rahastovastaava ät cluster.fi</p>
+          </div>
+
+          <div>
+          <Image src="/nerd.jpg"
+            width={500}
+            height={500}/>
+            <p>Name</p>
+            <p>Sihteeri</p>
+            <p>sihteeri ät cluster.fi</p>
+          </div>
+
+          <div>
+          <Image src="/nerd.jpg"
+            width={500}
+            height={500}/>
+            <p>Name</p>
+            <p>Kopo-sopo-vastaava</p>
+            <p>koposopo ät cluster.fi</p>
+          </div>
+
+          <div>
+          <Image src="/nerd.jpg"
+            width={500}
+            height={500}/>
+            <p>Name</p>
+            <p>Yrityssuhdevastaava</p>
+            <p>yritys ät cluster.fi</p>
+          </div>
+
+          <div>
+          <Image src="/nerd.jpg"
+            width={500}
+            height={500}/>
+            <p>Name</p>
+            <p>Tiedottaja</p>
+            <p>tiedottaja ät cluster.fi</p>
+          </div>
+
+          <div>
+          <Image src="/nerd.jpg"
+            width={500}
+            height={500}/>
+            <p>Name</p>
+            <p>Emäntä</p>
+            <p>isanta ät cluster.fi</p>
+          </div>
+
+          <div>
+          <Image src="/nerd.jpg"
+            width={500}
+            height={500}/>
+            <p>Name</p>
+            <p>Isäntä</p>
+            <p>isanta ät cluster.fi</p>
+          </div>
+
+          <div>
+          <Image src="/nerd.jpg"
+            width={500}
+            height={500}/>
+            <p>Name</p>
+            <p>Fuksivastaava, Yrityssuhdetoimihenkilö</p>
+            <p>fuksivastaava ät cluster.fi</p>
+            <p>yritys ät cluster.fi</p>
+          </div>
+
+        </div>
+
+        <p>Toimihenkilöt</p>
+
+        <div className="grid w-full grid-cols-4  gap-x-7 gap-y-20 ">
+          <div>
+          <Image src="/nerd.jpg"
+            width={500}
+            height={500}/>
+            <p>Name</p>
+            <p>Liikuntavastaava</p>
+            <p>liikunta ät cluster.fi</p>
+          </div>
+
+          <div>
+          <Image src="/nerd.jpg"
+            width={500}
+            height={500}/>
+            <p>Name</p>
+            <p>Quality-Assurance</p>
+            <p>qa ät cluster.fi</p>
+          </div>
+
+          <div>
+          <Image src="/nerd.jpg"
+            width={500}
+            height={500}/>
+            <p>Name</p>
+            <p>TEK-yhdyshenkilö</p>
+            <p>tek ät cluster.fi</p>
+          </div>
+
+          <div>
+          <Image src="/nerd.jpg"
+            width={500}
+            height={500}/>
+            <p>Name</p>
+            <p>Alumnivastaava</p>
+            <p>palumni ät cluster.fi</p>
+          </div>
+
+          <div>
+          <Image src="/nerd.jpg"
+            width={500}
+            height={500}/>
+            <p>Name</p>
+            <p>WWW- ja ATK-vastaava</p>
+            <p>www ät cluster.fi</p>
+          </div>
+
+          <div>
+          <Image src="/nerd.jpg"
+            width={500}
+            height={500}/>
+            <p>Name</p>
+            <p>WWW- ja ATK-kisälli</p>
+            <p>www ät cluster.fi</p>
+          </div>
+
+          <div>
+          <Image src="/nerd.jpg"
+            width={500}
+            height={500}/>
+            <p>Name</p>
+            <p>Yrityssuhdetoimihenkilö</p>
+            <p>yritys ät cluster.fi</p>
+          </div>
+
+          <div>
+          <Image src="/nerd.jpg"
+            width={500}
+            height={500}/>
+            <p>Name</p>
+            <p>Yrityssuhdetoimihenkilö</p>
+            <p>yritys ät cluster.fi</p>
+          </div>
+
+          <div>
+          <Image src="/nerd.jpg"
+            width={500}
+            height={500}/>
+            <p>Name</p>
+            <p>Häirintäyhdyshenkilö</p>
+            <p>hairintayhdyshenkilo ät cluster.fi</p>
+          </div>
+
+          <div>
+          <Image src="/nerd.jpg"
+            width={500}
+            height={500}/>
+            <p>Name</p>
+            <p>Häirintäyhdyshenkilö</p>
+            <p>hairintayhdyshenkilo ät cluster.fi</p>
+          </div>
+
+          <div>
+          <Image src="/nerd.jpg"
+            width={500}
+            height={500}/>
+            <p>Name</p>
+            <p>Quality-Assurance</p>
+            <p>qa ät cluster.fi</p>
+          </div>
+
+          <div>
+          <Image src="/nerd.jpg"
+            width={500}
+            height={500}/>
+            <p>Name</p>
+            <p>Valokuvaaja</p>
+            <p>valokuvaaja ät cluster.fi</p>
+          </div>
+
+
+          <div>
+          <Image src="/nerd.jpg"
+            width={500}
+            height={500}/>
+            <p>Name</p>
+            <p>Valokuvaaja</p>
+            <p>valokuvaaja ät cluster.fi</p>
+          </div>
+
+          <div>
+          <Image src="/nerd.jpg"
+            width={500}
+            height={500}/>
+            <p>Name</p>
+            <p>Haalarivastaava</p>
+            <p>haalarit ät cluster.fi</p>
+          </div>
+
+          <div>
+          <Image src="/nerd.jpg"
+            width={500}
+            height={500}/>
+            <p>Name</p>
+            <p>Alumnivastaava</p>
+            <p>alumni ät cluster.fi</p>
+          </div>
+
+          <div>
+          <Image src="/nerd.jpg"
+            width={500}
+            height={500}/>
+            <p>Name</p>
+            <p>Kiltahuonevastaava</p>
+            <p>kiltahuone ät cluster.fi</p>
+          </div>
+
+          <div>
+          <Image src="/nerd.jpg"
+            width={500}
+            height={500}/>
+            <p>Name</p>
+            <p>Kiltahuonevastaava</p>
+            <p>kiltahuone ät cluster.fi</p>
+          </div>
+
+          <div>
+          <Image src="/nerd.jpg"
+            width={500}
+            height={500}/>
+            <p>Name</p>
+            <p>KV-vastaava</p>
+            <p>kv ät cluster.fi</p>
+          </div>
+
+          <div>
+          <Image src="/nerd.jpg"
+            width={500}
+            height={500}/>
+            <p>Name</p>
+            <p>Tapahtumatoimija</p>
+            <p>isanta ät cluster.fi</p>
+          </div>
+
+          <div>
+          <Image src="/nerd.jpg"
+            width={500}
+            height={500}/>
+            <p>Name</p>
+            <p>Tapahtumatoimija</p>
+            <p>isanta ät cluster.fi</p>
+          </div>
+
+        </div>
+      </div>
+    </main>
+  );
+}
